8df732fe46afca82ec4da895ef7400e92ec9b447
Sean Murphy <sean@iamseanmurphy.com>
2024-10-29 13:33:34 +0000

    12.4

--- reviews-widgets-for-yelp/readme.txt
+++ reviews-widgets-for-yelp/readme.txt
@@ -5,7 +5,7 @@ Tags: yelp reviews, reviews, yelp, ratings, widget
 Requires at least: 3.0.1
 Tested up to: 6.6
 Requires PHP: 5.2
-Stable tag: 12.3
+Stable tag: 12.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Plugin Name: Widgets for Yelp Reviews
@@ -14,7 +14,7 @@ Plugin URI: https://wordpress.org/plugins/reviews-widgets-for-yelp/
 Author: Trustindex.io <support@trustindex.io>
 Author URI: https://www.trustindex.io/
 Text Domain: reviews-widgets-for-yelp
-Version: 12.3
+Version: 12.4
 
 Embed Yelp reviews fast and easily into your WordPress site. Increase SEO, trust and sales using Yelp reviews.
 
@@ -60,6 +60,7 @@ If you don’t have many Yelp reviews yet, one of the best strategies is to post
 * SEO boost
 * Dark website support
 * Use in any page builder: Gutenberg, Elementor, Page Origin, Beaver Builder, WPBakery, Divi, Classic, ...
+* Widget lazy load
 * Zero loading time
 * Use of assets stored in CDN
 * Linux & Windows (XAMPP, WAMP,...) compatible
--- reviews-widgets-for-yelp/trustindex-yelp-widget.class.php
+++ reviews-widgets-for-yelp/trustindex-yelp-widget.class.php
@@ -38,17 +38,14 @@ break;
 }
 }
 if (!$wasError && $instance['ti-widget-ID']) {
-echo $trustindex_pm_yelp->get_trustindex_widget($instance['ti-widget-ID']);
-}
-else if ($trustindex_pm_yelp->is_noreg_linked()) {
-echo $trustindex_pm_yelp->get_noreg_list_reviews();
-}
-else {
-echo $trustindex_pm_yelp->error_box_for_admins(sprintf(__("Please fill out <strong>all the required fields</strong> in the <a href='%s'>widget settings</a> page", 'trustindex-plugin'), admin_url('admin.php?page='.$trustindex_pm_yelp->get_plugin_slug().'/settings.php')));
+echo $trustindex_pm_yelp->renderWidgetFrontend($instance['ti-widget-ID']);
+} else if ($trustindex_pm_yelp->is_noreg_linked()) {
+echo $trustindex_pm_yelp->renderWidgetFrontend();
+} else {
+echo $trustindex_pm_yelp->frontEndErrorForAdmins(sprintf(__("Please fill out <strong>all the required fields</strong> in the <a href='%s'>widget settings</a> page", 'trustindex-plugin'), admin_url('admin.php?page='.$trustindex_pm_yelp->get_plugin_slug().'/settings.php')));
 }
 echo $after_widget;
-}
-else {
+} else {
 }
 }
 function form($instance)
