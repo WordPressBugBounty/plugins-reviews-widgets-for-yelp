e219b7d82424ea54592f0581b8bc082d3cbdf563
Sean Murphy <sean@iamseanmurphy.com>
2024-12-10 12:19:49 +0000

    12.4.7

--- reviews-widgets-for-yelp/uninstall.php
+++ reviews-widgets-for-yelp/uninstall.php
@@ -3,6 +3,6 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 die;
 }
 require_once plugin_dir_path( __FILE__ ) . 'trustindex-plugin.class.php';
-$trustindex_pm_yelp = new TrustindexPlugin_yelp("yelp", __FILE__, "12.4.6", "Widgets for Yelp Reviews", "Yelp");
+$trustindex_pm_yelp = new TrustindexPlugin_yelp("yelp", __FILE__, "12.4.7", "Widgets for Yelp Reviews", "Yelp");
 $trustindex_pm_yelp->uninstall();
 ?>
